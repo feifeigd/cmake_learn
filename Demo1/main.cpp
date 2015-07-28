a296a6fefaa53f775ef1e5d5f757d259142b67c2
luofei <13632323692@139.com>
2015-07-28 12:39:00 +0800

    添加配置文件

--- Demo1/main.cpp
+++ Demo1/main.cpp
@@ -1,5 +1,11 @@
 
+#include "config.h"
+
+#ifdef USE_MYMATH
 #include <MathFunctions.h>
+#else
+#include <cmath>
+#endif // USE_MYMATH
 
 #include <cstdlib> // atof atoi
 #include <iostream>
@@ -14,7 +20,13 @@ int main(int argc, char* argv[]){
 	}
 	double base = atof(argv[1]);
 	int exponent = atoi(argv[2]);
+#ifdef USE_MYMATH
+	cout << "Now we use our own Math library.";
 	double result = power(base, exponent);
+#else
+	cout << "Now we use the standard library.";
+	double result = pow(base, exponent);
+#endif // USE_MYMATH
 	cout << base << " ^ " << exponent << " is " << result << endl;
 	return 0;
 }
