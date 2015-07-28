f9d5f879c7101d3ea208e22f055dacfe91c88c89
luofei <13632323692@139.com>
2015-07-28 12:02:59 +0800

    添加功能代码

--- Demo1/main.cpp
+++ Demo1/main.cpp
@@ -1,4 +1,23 @@
 
+#include <iostream>
+using namespace std;
+
+double power(double base, int exponent){
+	int result = base;
+	for (int i = 1; i < exponent; ++i)
+		result *= base;
+	return result;
+}
+
 int main(int argc, char* argv[]){
+	if (argc < 3)
+	{
+		cout << "Usage: " << argv[0] << " base exponent." << endl; 
+		return 1;
+	}
+	double base = atof(argv[1]);
+	int exponent = atoi(argv[2]);
+	double result = power(base, exponent);
+	cout << base << " ^ " << exponent << " is " << result << endl;
 	return 0;
 }
