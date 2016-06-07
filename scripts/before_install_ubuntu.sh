a328834a446fdef79c01bf501dda2a4961b0af04
luofei <13632323692@139.com>
2016-06-07 16:03:52 +0800

    不安装cmake了

--- scripts/before_install_ubuntu.sh
+++ scripts/before_install_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-sudo apt-get remove -y cmake # 卸载旧版本的
-sudo add-apt-repository -y ppa:george-edison55/precise-backports
-sudo apt-get update 
-sudo apt-get install cmake
+# travis-ci.org已经给安装上了3.2.2的cmake了不必安装了
+# sudo apt-get remove -y cmake # 卸载旧版本的
+# sudo add-apt-repository -y ppa:george-edison55/precise-backports
+# sudo apt-get update 
+# sudo apt-get install cmake
